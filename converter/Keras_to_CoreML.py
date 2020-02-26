d735a5b0d71dcdd15c0ae0564ac63d98abe677a0
Takashi Shirakawa <52600509+tkshirakawa@users.noreply.github.com>
2020-02-26 23:18:57 +0900

    Add drop_out remover

--- converter/Keras_to_CoreML.py
+++ converter/Keras_to_CoreML.py
@@ -1,33 +1,8 @@
 '''
-    Copyright (c) 2020, Takashi Shirakawa. All rights reserved.
+    Copyright (c) 2019-2020, Takashi Shirakawa. All rights reserved.
     e-mail: tkshirakawa@gmail.com
 
     Released under the BSD 3-Clause License
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
 
@@ -35,6 +10,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys
 import os
+import warnings
+warnings.filterwarnings('ignore')
 
 if sys.argv[1] == '-h':
     print('### Help for argv ###')
@@ -169,20 +146,31 @@ def convert_keras_to_mlmodel(keras_model_path, coreml_model_path):
     import importlib.machinery as imm
     from coremltools.converters.keras._keras_converter import convertToSpec
     from coremltools.models import MLModel, _MLMODEL_FULL_PRECISION, _MLMODEL_HALF_PRECISION
-    from coremltools.models.utils import convert_double_to_float_multiarray_type
+#    from coremltools.models.utils import convert_double_to_float_multiarray_type
+
+    from keras.models import load_model
+    from kerassurgeon.operations import delete_layer
+
 
     sys.path.append(os.path.dirname(sys.argv[4]))
 
 
     # Import neural network code
-    NN_name = os.path.splitext(os.path.basename(sys.argv[3]))[0]
-    NN = imm.SourceFileLoader(NN_name, sys.argv[3]).load_module()
+    NN_file_name = os.path.splitext(os.path.basename(sys.argv[3]))[0]
+    NN = imm.SourceFileLoader(NN_file_name, sys.argv[3]).load_module()
+    
+    try:    NN_model_name = NN.Model_Name()
+    except: NN_model_name = NN_file_name
+    
+    try:    NN_model_description = NN.Model_Description()
+    except: NN_model_description = None
+
 
     # Load custom layers if implemented in each Keras model
     # Take care the imported NN may not have Custom_Layers() def, so try and catch except.
     # The type is a dictionary. The keys are supposed to be same as the corresponding values (=defs).
-    try:    keras_custom_layers = NN.Custom_Layers()
-    except: keras_custom_layers = {}
+    try:    NN_custom_layers = NN.Custom_Layers()
+    except: NN_custom_layers = {}
 
 
     # Import Train.py to get custom loss and metrics
@@ -192,29 +180,49 @@ def convert_keras_to_mlmodel(keras_model_path, coreml_model_path):
     custom_loss = Train_py.get_loss()
     custom_metrics = Train_py.get_metrics()
 
+    kpt, kex = os.path.splitext(keras_model_path)
+    keras_model_path_temp = kpt + '_temp' + kex
 
     print('----------------------------------------------------------')
-    print('NN model name:')
-    print(NN_name)
-    print('NN model file path:')
-    print(sys.argv[3])
+    print('NN model file path: {}'.format(sys.argv[3]))
+    print('NN model name: {}'.format(NN_model_name))
+    print('NN model description: {}'.format(NN_model_description))
     print('NN custom layers:')
-    print(keras_custom_layers)
-
-    print('Training file path and loss/metrics used in it:')
+    print(NN_custom_layers)
+    print('Training file path and loss/metrics used:')
     print(sys.argv[4])
     print(custom_loss)
     print(custom_metrics)
 
     print('----------------------------------------------------------')
-    print('Keras model file:')
-    print(keras_model_path)
-    print('CoreML model file:')
-    print(coreml_model_path)
+    print('Keras model file: {}'.format(keras_model_path))
+    print('Keras model file temp: {}'.format(keras_model_path_temp))
+    print('CoreML model file: {}'.format(coreml_model_path))
 
     print('----------------------------------------------------------')
     print('Keras custom layers implemented in AIAS for this code:')
     for k in conversion_func_in_AIAS: print(k)
+
+
+    # Deleting Dropout layers from the Keras model to be converted
+    # Because the layers will cause unknown conversion failures in coremltools
+    keras_model = load_model(keras_model_path, custom_objects=dict(**custom_loss, **custom_metrics, **NN_custom_layers), compile=False)
+
+    print('----------------------------------------------------------')
+    keras_model.summary()
+
+    idp = 1
+    del_prefix = 'dropout'
+    while True:
+        try:    layer = keras_model.get_layer('{}_{}'.format(del_prefix, idp))
+        except: break
+        print('Deleting layer: {}_{}'.format(del_prefix, idp))
+        keras_model = delete_layer(model=keras_model, layer=layer, copy=False)
+        idp += 1
+
+    keras_model.summary()
+    print('Saving temporary Keras model: {}'.format(keras_model_path_temp))
+    keras_model.save(keras_model_path_temp)
 
 
     # Construct custom layers and conversion functions
@@ -222,12 +230,12 @@ def convert_keras_to_mlmodel(keras_model_path, coreml_model_path):
     custom_conversion_func = {}
     print('----------------------------------------------------------')
 
-    if keras_custom_layers is not None:
+    if NN_custom_layers is not None:
         print('Custom layers in this Keras model:')
-        for keras_layer_key in keras_custom_layers:
+        for keras_layer_key in NN_custom_layers:
             if keras_layer_key in conversion_func_in_AIAS:
                 print(keras_layer_key + ' - available')
-                custom_layers[keras_layer_key]          = keras_custom_layers[keras_layer_key]
+                custom_layers[keras_layer_key]          = NN_custom_layers[keras_layer_key]
                 custom_conversion_func[keras_layer_key] = conversion_func_in_AIAS[keras_layer_key]
             else:
                 print(keras_layer_key + ' - unavailable')
@@ -250,7 +258,7 @@ def convert_keras_to_mlmodel(keras_model_path, coreml_model_path):
 
     # Convert
     # Do not change the input_names/output_names because they are used to identify input/output layers in Keras code
-    spec = convertToSpec( keras_model_path,
+    spec = convertToSpec( keras_model_path_temp,
                           input_names                 = 'input',
                           output_names                = 'output',
                           add_custom_layers           = True,
@@ -263,7 +271,7 @@ def convert_keras_to_mlmodel(keras_model_path, coreml_model_path):
     # Set descriptions
     model.author = 'Takashi Shirakawa'
     model.license = '(C) 2019-2020, Takashi Shirakawa. All right reserved.'
-    model.short_description = 'CoreML model for A.I.Segmentation'
+    model.short_description = NN_model_name + ' for A.I.Segmentation'
     model.input_description['input'] = 'Input grayscale image must be 8-bit depth (0-255 tones) per pixel with the size of 200x200 pixels.'
     model.output_description['output'] = 'Predicted images (segmentation results) will be saved in the same format.'
 

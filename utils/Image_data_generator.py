256521dbada5671c67817047a8270ef9b1b1c69b
Takashi Shirakawa <52600509+tkshirakawa@users.noreply.github.com>
2020-09-10 16:15:43 +0900

    Sep. 10, 2020

--- utils/Image_data_generator.py
+++ utils/Image_data_generator.py
@@ -1,8 +1,16 @@
 '''
     Copyright (c) 2019-2020, Takashi Shirakawa. All rights reserved.
     e-mail: tkshirakawa@gmail.com
+    
+    
+    Released under the MIT license.
+    https://opensource.org/licenses/mit-license.php
 
-    Released under the BSD 3-Clause License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 
@@ -14,9 +22,14 @@ import h5py
 import math
 import numpy as np
 
-from keras import backend as K
 
-##### For TensorFlow v2.0 #####
+import tensorflow as tf
+from keras import backend as K
+# from keras.utils import Sequence
+
+##### For TensorFlow v2 #####
+# import tensorflow as tf
+# from tensorflow import keras
 # from tensorflow.keras import backend as K
 # from tensorflow.keras.utils import Sequence
 
@@ -34,9 +47,9 @@ from keras import backend as K
 #####################################################################
 # Read a list (first row for header, two column for data) from .csv file, and load images.
 # You should not shuffle the rows in .csv file, or image loading speed will be decreased.
-# Images must be 200x200 gray-scale without alpha values.
+# Images must be gray-scale without alpha values.
 
-# class ImageDataGenerator_CSV_with_Header(Sequence):       ##### For TensorFlow v2.0 #####
+# class ImageDataGenerator_CSV_with_Header(Sequence):       ##### For TensorFlow v2 #####
 class ImageDataGenerator_CSV_with_Header():
 
     def __init__(self, dataname, datafile, batch_size=32, rescale=1.0, shuffle=True):
@@ -46,21 +59,26 @@ class ImageDataGenerator_CSV_with_Header():
         with open(datafile, 'r', newline='', encoding='utf-8_sig') as f:
             self.length_ = sum(1 for line in f) - 1     # !!! First row is a header, skip !!!
 
-        print('Data name      : {0}'.format(dataname))
-        print('Path           : {0}'.format(datafile))
-        print('Counts         : {0}'.format(self.length_))
-        print('Batch size     : {0}'.format(self.batch_size))
-        print('Pixel scaling  : {0}'.format(self.rescale))
-
         # Open the CSV file
         with open(datafile, 'r', newline='', encoding='utf-8_sig') as f:
             reader = csv.reader(f)
             next(reader)                                # !!! First row is a header, skip !!!
             paths = [row for row in reader]
 
+        # Image dimensions
+        img_check = cv2.imread(paths[0][0], -1)
+        height, width = img_check.shape[:2]
+
+        print('Data name      : {0}'.format(dataname))
+        print('Path           : {0}'.format(datafile))
+        print('Counts         : {0}'.format(self.length_))
+        print('H,W            : {0}x{1}'.format(height, width))
+        print('Batch size     : {0}'.format(self.batch_size))
+        print('Pixel scaling  : {0}'.format(self.rescale))
+
         # Store image data as 'uint8' to save memory consumption
-        self.X_ = np.empty((self.length_, 200, 200, 1), dtype=np.uint8)
-        self.Y_ = np.empty((self.length_, 200, 200, 1), dtype=np.uint8)
+        self.X_ = np.empty((self.length_, height, width, 1), dtype=np.uint8)
+        self.Y_ = np.empty((self.length_, height, width, 1), dtype=np.uint8)
         index = np.arange(self.length_)        # [0,1,2, ...]
         if shuffle:
             np.random.shuffle(index)
@@ -99,18 +117,18 @@ class ImageDataGenerator_CSV_with_Header():
         print('\r  Loaded  : %d ' % self.length_ + '#'*100)
 
 
-    ##### For TensorFlow v2.0 #####
-    # # Number of batch in the Sequence.
+    # Number of batch in the Sequence       ##### For TensorFlow v2 #####
     # def __len__(self): return math.ceil(self.length_ / self.batch_size)
 
-    # # Gets batch at position index.
+
+    # Gets batch at position index          ##### For TensorFlow v2 #####
     # def __getitem__(self, index):
     #     X_batch = K.cast_to_floatx(self.rescale * self.X_[index*self.batch_size : (index+1)*self.batch_size])
     #     Y_batch = K.cast_to_floatx(self.rescale * self.Y_[index*self.batch_size : (index+1)*self.batch_size])
     #     return (X_batch, Y_batch)
 
 
-    def length(self): return self.length_
+    def datalength(self): return self.length_
 
 
     def getdata(self):
@@ -133,7 +151,7 @@ class ImageDataGenerator_CSV_with_Header():
 # Image data generator for h5 file
 #####################################################################
 
-# class ImageDataGenerator_h5_Dataset(Sequence):    ##### For TensorFlow v2.0 #####
+# class ImageDataGenerator_h5_Dataset(Sequence):        ##### For TensorFlow v2 #####
 class ImageDataGenerator_h5_Dataset():
 
     def __init__(self, dataname, datafile, batch_size=32, rescale=1.0):
@@ -157,18 +175,18 @@ class ImageDataGenerator_h5_Dataset():
         f.close()
 
 
-    ##### For TensorFlow v2.0 #####
-    # # Number of batch in the Sequence.
+    # Number of batch in the Sequence       ##### For TensorFlow v2 #####
     # def __len__(self): return math.ceil(self.length_ / self.batch_size)
 
-    # # Gets batch at position index.
+
+    # Gets batch at position index          ##### For TensorFlow v2 #####
     # def __getitem__(self, index):
     #     X_batch = K.cast_to_floatx(self.rescale * self.X_[index*self.batch_size : (index+1)*self.batch_size])
     #     Y_batch = K.cast_to_floatx(self.rescale * self.Y_[index*self.batch_size : (index+1)*self.batch_size])
     #     return (X_batch, Y_batch)
 
 
-    def length(self): return self.length_
+    def datalength(self): return self.length_
 
 
     def getdata(self):
--- utils/Loss_and_metrics.py
+++ utils/Loss_and_metrics.py
@@ -1,191 +1,172 @@
 '''
     Copyright (c) 2019-2020, Takashi Shirakawa. All rights reserved.
     e-mail: tkshirakawa@gmail.com
+    
+    
+    Released under the MIT license.
+    https://opensource.org/licenses/mit-license.php
 
-    Released under the BSD 3-Clause License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 
-##### For TensorFlow v2.0 #####
-# import tensorflow as tf
-# from tensorflow.keras import backend as K
-# from tensorflow.keras.losses import mean_squared_error, mean_absolute_error, hinge, squared_hinge, logcosh
-
 import tensorflow as tf
+
+# from tensorflow.python.framework import ops
+# from tensorflow.python.ops import math_ops as mop
+
 from keras import backend as K
-from keras.losses import mean_squared_error, hinge, squared_hinge, logcosh, binary_crossentropy, kullback_leibler_divergence
+from keras.losses import mean_squared_error, logcosh, kullback_leibler_divergence
+
+##### For TensorFlow v2 #####
+# from tensorflow import keras
+# from tensorflow.keras import backend as K
+# from tensorflow.keras.losses import mean_squared_error, logcosh, kullback_leibler_divergence
 
 
 
 
-num_classes = -1
-def set_num_classes(n=1):
-    global num_classes
-    num_classes = n
+# Axis for image
+# IMPORTANT to complete calculation in each image plane in several batches or samples 
+# NOTE: K.image_data_format() must be 'channels_last'! = (samples, height, width, channels) *channels=1 for grayscale images in this model
+# NOTE: In the following metrics and losses, the tensor 'y_XXXX' has the dimension of (batch size, height, width, channels=1)
+img = (-3,-2,-1)
+
+
+# Clip values between 0 and 1 with keeping its gradient
+# 0.0 if x < 0.0; 1.0 if x > 1.0; x if 0.0 <= x <= 1.0
+def hard_sigmoid_clip(val): return tf.keras.activations.hard_sigmoid(5.0 * (val - 0.5))
 
 
 
 
-def mean_squared_error_loss(y_true, y_pred): return mean_squared_error(y_true, y_pred)
+###
+### Metrics
+###
 
-def mean_absolute_error_loss(y_true, y_pred): return mean_absolute_error(y_true, y_pred)
-
-
-def dilation(tensor, size=5): return K.pool2d(tensor, pool_size=(size,size), strides=(1,1), padding="same", pool_mode='max')
-
-def erosion(tensor, size=5): return -K.pool2d(-tensor, pool_size=(size,size), strides=(1,1), padding="same", pool_mode='max')
-
-def morphology_gradient(tensor, size=5): return dilation(tensor, size=size) - erosion(tensor, size=size)
-
-
-def proc_tensors(y_true, y_pred):
-    if not K.is_tensor(y_pred):
-        y_pred = K.constant(y_pred)
-    # y_true = K.clip(K.cast(y_true >= 0.5, y_pred.dtype), K.epsilon(), 1.)
-    # y_pred = K.clip(K.cast(y_pred >= 0.5, y_pred.dtype), K.epsilon(), 1.)
-    y_true = K.clip(y_true, K.epsilon(), 1.)
-    y_pred = K.clip(y_pred, K.epsilon(), 1.)
-    ch = -100
-    if num_classes == 1:
-        ch = None
-    elif K.ndim(y_pred) == 4:
-        if K.image_data_format() == 'channels_first':  ch = 1       # batch_size(N), ch(C), row(H), col(W)
-        elif K.image_data_format() == 'channels_last': ch = -1      # batch_size(N), row(H), col(W), ch(C)
-    return y_true, y_pred, ch
+# IoU: 0.0 [bad] - 1.0 [good]
+def iou_score(y_true, y_pred):
+    t = hard_sigmoid_clip(y_true)       # Dimension = (batch size, height, width, channels=1)
+    p = hard_sigmoid_clip(y_pred)
+    u = hard_sigmoid_clip(t + p)
+    truth = K.sum(t, axis=img)          # Dimension = (batch size)
+    predc = K.sum(p, axis=img)
+    union = K.sum(u, axis=img)
+    count = K.max(t, axis=img)
+    return K.sum((truth + predc - union) / (union + K.epsilon())) / K.sum(count)    # Scalar: The mean value alongside the batches where y_true is not zero
 
 
+# Dice: 0.0 [bad] - 1.0 [good]
+def dice_coef(y_true, y_pred):
+    t = hard_sigmoid_clip(y_true)
+    p = hard_sigmoid_clip(y_pred)
+    u = hard_sigmoid_clip(t + p)
+    truth = K.sum(t, axis=img)
+    predc = K.sum(p, axis=img)
+    union = K.sum(u, axis=img)
+    count = K.max(t, axis=img)
+    t_or_p = truth + predc
+    t_and_p_2 = 2.0 * (t_or_p - union)
+    return K.sum(t_and_p_2 / (t_or_p + K.epsilon())) / K.sum(count)
 
 
-####################################################################################################
-######      IoU
-####################################################################################################
-
-def mean_iou(y_true, y_pred):
-    y_true, y_pred, ch = proc_tensors(y_true, y_pred)
-    intersection = K.sum(K.sqrt(y_true * y_pred), axis=ch)      # y_XXXX is a kind of 'area'. So, the multiplied value should be reverted to its root.
-    union = K.sum(y_true, axis=ch) + K.sum(y_pred, axis=ch) - intersection
-    return K.mean(intersection / union, axis=ch)
-
-
-def mean_iou_loss(y_true, y_pred):
-    return 1. - mean_iou(y_true, y_pred)
-
-
-def mean_iou_MSE_loss(y_true, y_pred):
-    return mean_iou_loss(y_true, y_pred) + mean_squared_error(y_true, y_pred)
-
-
-def mean_iou_MSE2_loss(y_true, y_pred):
-    return 0.5 * mean_iou_loss(y_true, y_pred) + 1.5 * mean_squared_error(y_true, y_pred)
-
-
-def mean_iou_BCE_loss(y_true, y_pred):
-    return mean_iou_loss(y_true, y_pred) + binary_crossentropy(y_true, y_pred)
-
-
-def mean_iou_MSE_border_MSE_loss(y_true, y_pred):
-    y_true, y_pred, _ = proc_tensors(y_true, y_pred)
-    y_true_mol = morphology_gradient(y_true, size=11)
-    y_pred_mol = morphology_gradient(y_pred, size=11)
-    return mean_iou_loss(y_true, y_pred) + mean_squared_error(y_true, y_pred) + mean_squared_error(y_true_mol, y_pred_mol)
-
-
-def mean_iou_MSE_border_BCE_loss(y_true, y_pred):
-    y_true, y_pred, _ = proc_tensors(y_true, y_pred)
-    y_true_mol = morphology_gradient(y_true, size=11)
-    y_pred_mol = morphology_gradient(y_pred, size=11)
-    return mean_iou_loss(y_true, y_pred) + mean_squared_error(y_true, y_pred) + binary_crossentropy(y_true_mol, y_pred_mol)
-
-
-def mean_iou_MSE_border_KLD_loss(y_true, y_pred):
-    y_true, y_pred, _ = proc_tensors(y_true, y_pred)
-    y_true_mol = morphology_gradient(y_true, size=11)
-    y_pred_mol = morphology_gradient(y_pred, size=11)
-    return mean_iou_loss(y_true, y_pred) + mean_squared_error(y_true, y_pred) + kullback_leibler_divergence(y_true_mol, y_pred_mol)
-
-
-def mean_iou_SVM_loss(y_true, y_pred):
-    return mean_iou_loss(y_true, y_pred) + hinge(y_true, y_pred)
-
-
-def mean_iou_SSVM_loss(y_true, y_pred):
-    return mean_iou_loss(y_true, y_pred) + squared_hinge(y_true, y_pred)
-
-
-def mean_iou_LGC_loss(y_true, y_pred):
-    return mean_iou_loss(y_true, y_pred) + logcosh(y_true, y_pred)
-
-
-
-
-####################################################################################################
-######      RoU: residual over union
-####################################################################################################
-
-# RoU: residual over union
+# RoU: residual over union: 0.0 [good] - 1.0 [bad]
 # The residual is uncovered residual area in grand-truth. RoU = grand-truth - intersection
-def mean_rou(y_true, y_pred):
-    y_true, y_pred, ch = proc_tensors(y_true, y_pred)
-    true_sum = K.sum(y_true, axis=ch)
-    intersection = K.sum(K.sqrt(y_true * y_pred), axis=ch)      # y_XXXX is a kind of 'area'. So, the multiplied value should be reverted to its root.
-    union = true_sum + K.sum(y_pred, axis=ch) - intersection
-    return K.mean((true_sum - intersection) / union, axis=ch)
+def rou(y_true, y_pred):
+    t = hard_sigmoid_clip(y_true)
+    p = hard_sigmoid_clip(y_pred)
+    u = hard_sigmoid_clip(t + p)
+    truth = K.sum(t, axis=img)
+    predc = K.sum(p, axis=img)
+    union = K.sum(u, axis=img)
+    count = K.max(t, axis=img)
+    return K.sum((union - predc) / (union + K.epsilon())) / K.sum(count)
 
 
-# mean_iou_rou = (mean_iou + (1 - mean_rou)) / 2    range: 0 - 1
-def mean_iou_rou(y_true, y_pred):
-    return 0.5 * (mean_iou(y_true, y_pred) + (1. - mean_rou(y_true, y_pred)))
+# FPoET: false positives on empty truth: 0.0 [good] - 1.0 [bad]
+def fpoet(y_true, y_pred):
+    empty = 1.0 - K.max(hard_sigmoid_clip(y_true), axis=img)        # Dimension = (batch size), 1.0 if a slice of y_true is empty (= all the pixels are zero)
+    predc = K.sum(hard_sigmoid_clip(y_pred), axis=img)              # Dimension = (batch size), the area of prediction pixels in each slice of y_true
+    area  = K.sum(K.ones_like(K.max(y_true, axis=-1), dtype='float32'), axis=(-2,-1))   # Dimension = (batch size)
+    return K.sum(empty * predc / area) / (K.sum(empty) + K.epsilon())
 
 
-def mean_iou_rou_MSE_loss(y_true, y_pred):
-    return 1. - mean_iou_rou(y_true, y_pred) + mean_squared_error(y_true, y_pred)
 
 
+###
+### Losses
+###
 
+def mean_squared_error_loss(y_true, y_pred):
+    t = hard_sigmoid_clip(y_true)                       # Dimension = (batch size, height, width, channels=1)
+    p = hard_sigmoid_clip(y_pred)
+    return K.mean(mean_squared_error(t, p), axis=None)  # Scalar: The mean value alongside all the axes
 
-####################################################################################################
-######      Huber loss
-####################################################################################################
 
 def Huber_loss(y_true, y_pred):
-    return tf.compat.v1.losses.huber_loss(y_true, y_pred, weights=25.0, delta=0.5)
+    t = hard_sigmoid_clip(y_true)
+    p = hard_sigmoid_clip(y_pred)
+    return K.mean(tf.compat.v1.losses.huber_loss(t, p), axis=None)
 
 
-def mean_iou_Huber_loss(y_true, y_pred):
-    return mean_iou_loss(y_true, y_pred) + Huber_loss(y_true, y_pred)
+def LogCosh_loss(y_true, y_pred):
+    t = hard_sigmoid_clip(y_true)
+    p = hard_sigmoid_clip(y_pred)
+    return K.mean(logcosh(t, p), axis=None)
 
 
+def kullback_leibler_divergence_loss(y_true, y_pred):
+    t = hard_sigmoid_clip(y_true)
+    p = hard_sigmoid_clip(y_pred)
+    return K.mean(kullback_leibler_divergence(t, p), axis=None)
 
 
-####################################################################################################
-######      Dice
-####################################################################################################
-
-def dice_coef(y_true, y_pred):
-    y_true, y_pred, ch = proc_tensors(y_true, y_pred)
-    numerator = 2. * K.sum(K.sqrt(y_true * y_pred), axis=ch)        # y_XXXX is a kind of 'area'. So, the multiplied value should be reverted to its root.
-    denominator = K.sum(y_true, axis=ch) + K.sum(y_pred, axis=ch)
-    return K.mean(numerator / denominator, axis=ch)
+def MSE_loss_w_iou_score(y_true, y_pred):
+    return 1.0 - iou_score(y_true, y_pred) + mean_squared_error_loss(y_true, y_pred)
 
 
-def dice_coef_loss(y_true, y_pred):
-    return 1. - dice_coef(y_true, y_pred)
+def MSE_loss_w_iou_score_fpoet(y_true, y_pred):
+    return 1.0 - iou_score(y_true, y_pred) + fpoet(y_true, y_pred) + mean_squared_error_loss(y_true, y_pred)
 
 
-def dice_coef_MSE_loss(y_true, y_pred):
-    return dice_coef_loss(y_true, y_pred) + mean_squared_error(y_true, y_pred)
+def MSE_loss_w_dice_coef(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred) + mean_squared_error_loss(y_true, y_pred)
 
 
-def dice_coef_SVM_loss(y_true, y_pred):
-    return dice_coef_loss(y_true, y_pred) + hinge(y_true, y_pred)
+def MSE_loss_w_dice_coef_fpoet(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred) + fpoet(y_true, y_pred) + mean_squared_error_loss(y_true, y_pred)
 
 
-def dice_coef_SSVM_loss(y_true, y_pred):
-    return dice_coef_loss(y_true, y_pred) + squared_hinge(y_true, y_pred)
+def Huber_loss_w_iou_score(y_true, y_pred):
+    return 1.0 - iou_score(y_true, y_pred) + Huber_loss(y_true, y_pred)
 
 
-def dice_coef_LGC_loss(y_true, y_pred):
-    return dice_coef_loss(y_true, y_pred) + logcosh(y_true, y_pred)
+def LogCosh_loss_w_iou_score(y_true, y_pred):
+    return 1.0 - iou_score(y_true, y_pred) + LogCosh_loss(y_true, y_pred)
+
+
+def LogCosh_loss_w_iou_score_fpoet(y_true, y_pred):
+    return 1.0 - iou_score(y_true, y_pred) + fpoet(y_true, y_pred) + LogCosh_loss(y_true, y_pred)
+
+
+def LogCosh_loss_w_dice_coef(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred) + LogCosh_loss(y_true, y_pred)
+
+
+def LogCosh_loss_w_dice_coef_fpoet(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred) + fpoet(y_true, y_pred) + LogCosh_loss(y_true, y_pred)
+
+
+def KLD_loss_w_iou_score(y_true, y_pred):
+    return 1.0 - iou_score(y_true, y_pred) + kullback_leibler_divergence_loss(y_true, y_pred)
+
+
+def KLD_loss_w_dice_coef(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred) + kullback_leibler_divergence_loss(y_true, y_pred)
 
 
 
